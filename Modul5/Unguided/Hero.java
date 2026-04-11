87b5b9260819f3dfd17c8ae7a3ecd6c886fc1064
kukingkux <cbmamangbae@gmail.com>
2026-04-11 22:42:43 +0700

    modul5_program_done

--- Modul5/Unguided/Hero.java
+++ Modul5/Unguided/Hero.java
@@ -4,7 +4,7 @@ public class Hero {
     private String nama;
     private int hp, mana, damage;
     private Boolean checkHp, checkMana, checkDamage;
-
+    private int maxHp;
 
     // Constructor
     public Hero(
@@ -14,8 +14,8 @@ public class Hero {
         int damage
     ) {
         checkHp = hp >= 0 && hp <= 100;
-        checkMana = mana >= 0 && hp <= 100;
-        checkDamage = damage >= 0 && hp <= 15;
+        checkMana = mana >= 0 && mana <= 100;
+        checkDamage = damage >= 0 && damage <= 15;
         if (
             checkHp &&
             checkMana &&
@@ -25,6 +25,7 @@ public class Hero {
             this.hp = hp;
             this.mana = mana;
             this.damage = damage;
+            this.maxHp = hp;
         } else {
             if (!checkHp && checkMana && checkDamage) {
                 System.out.println("HP harus bernilai 0-100 !");
@@ -36,9 +37,59 @@ public class Hero {
                 System.out.println("HP dan Mana harus bernilai 0-100 !");
                 System.out.println("Damage harus bernilai 0-15 !");
             }
-            return;
         }
     }
 
+    public void basicAttack(Hero target) {
+        target.hp -= this.damage;
+        if (target.hp < 0) {
+            target.hp = 0;
+        }
+        System.out.println("["+this.nama+"] Melancarkan Basic Attack ke " + target.nama + " (Damage: " + this.damage + ")");
+        System.out.println();
+    }
 
+    public void skillAttack(Hero target) {
+        if (this.mana >= 25) {
+            target.hp -= this.damage*2;
+            this.mana -= 25;
+        
+            if (target.hp < 0) {
+                target.hp = 0;
+            }
+            System.out.println("["+this.nama+"] Melancarkan Skill ke " + target.nama + " (Damage: " + this.damage*2 + ")");
+            System.out.println();
+        } else {
+            System.out.println("["+this.nama+"] Mana tidak cukup untuk melancarkan skill.");
+            System.out.println();
+        }
+    }
+
+    public void regenHp() {
+        if (this.hp < maxHp && this.hp > 0) {
+            this.hp += 15;
+            if (this.hp > 100) {
+                this.hp = maxHp;
+            }
+            System.out.println("["+this.nama+"] Regenerasi, +15 Hp");
+            System.out.println();
+        } else {
+            System.out.println("["+this.nama+"] Tidak dapat regenerasi (mati).");
+            System.out.println();
+        }
+        // return target.hp;
+    }
+
+    public void attributeStatus() {
+        System.out.println("Nama : " + this.nama);
+        System.out.println("Current Hp / Max Hp : " + this.hp + " / " + maxHp);
+        System.out.println("Mana : " + this.mana);
+        System.out.println("Damage: " + this.damage);
+        if (this.hp == 0) {
+            System.out.println("Status: DEAD");
+        } else if (this.hp > 0) {
+            System.out.println("Status: ALIVE");
+        }
+        System.out.println();
+    }
 }
